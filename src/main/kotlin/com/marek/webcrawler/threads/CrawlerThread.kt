3e16981a22473626924c4c2df1923ec50b744119
Qverkk <membersy@gmail.com>
2019-03-13 15:13:34 +0100

    Revert "Utilize remove prefix"
    This reverts commit 2cab9f95cd807ed7478edfccdf930f262efd5221.

--- src/main/kotlin/com/marek/webcrawler/threads/CrawlerThread.kt
+++ src/main/kotlin/com/marek/webcrawler/threads/CrawlerThread.kt
@@ -31,7 +31,21 @@ abstract class CrawlerThread(val number: Int) : Thread() {
 }
 
 fun getDomain(url: String): String {
-    return url
-            .removePrefix("https://")
-            .removePrefix("http://")
+    if (url.startsWith("https://")) {
+        return replaceStrings(url, "https://")
+    } else if (url.startsWith("http://")) {
+        return replaceStrings(url, "http://")
+    }
+    return ""
+}
+
+private fun replaceStrings(url: String, replacer: String): String {
+    val tmp = url.replace(replacer, "")
+    var result = ""
+    for (c in tmp) {
+        if (c == '/')
+            break
+        result += c
+    }
+    return result
 }
