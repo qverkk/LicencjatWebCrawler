2cab9f95cd807ed7478edfccdf930f262efd5221
polish <polishcivil@gmail.com>
2019-03-13 02:30:42 +0100

    Utilize remove prefix

--- src/main/kotlin/com/marek/webcrawler/threads/CrawlerThread.kt
+++ src/main/kotlin/com/marek/webcrawler/threads/CrawlerThread.kt
@@ -40,21 +40,7 @@ class CrawlerThread(val number: Int, val mainPresenter: MainPresenter) : Thread(
 }
 
 fun getDomain(url: String): String {
-    if (url.startsWith("https://")) {
-        return replaceStrings(url, "https://")
-    } else if (url.startsWith("http://")) {
-        return replaceStrings(url, "http://")
-    }
-    return ""
-}
-
-private fun replaceStrings(url: String, replacer: String): String {
-    val tmp = url.replace(replacer, "")
-    var result = ""
-    for (c in tmp) {
-        if (c == '/')
-            break
-        result += c
-    }
-    return result
+    return url
+            .removePrefix("https://")
+            .removePrefix("http://")
 }
